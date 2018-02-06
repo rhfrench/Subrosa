42b760953503247f450df28868da8f26717e2ed2
RFRENCHLAPTOP\ryanf <ryanfrench@ymail.com>
2018-02-05 19:58:35 -0700

    Initial commit

---
+++ Subrosa/Subrosa/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Subrosa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subrosa/Subrosa/StorageServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Subrosa
+{
+    public class StorageServices
+    {
+        private StreamWriter _writer { get; set; }
+
+        public void SetupWriter(string location, string fileName)
+        {
+            if (String.IsNullOrWhiteSpace(location))
+                location = @"C:\";
+
+            if (String.IsNullOrWhiteSpace(fileName))
+                fileName = "log.txt";
+
+            this._writer = new StreamWriter(location + fileName);
+            this._writer.WriteLine("Setup Complete" + "\n");
+        }
+
+    }
+}
